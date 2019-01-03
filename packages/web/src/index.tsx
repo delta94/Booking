a6afc55899cf5470ac445bcaf12f05769bb2b11b
Marcin Miler <marcin4951@gmail.com>
2019-01-03 20:01:10 +0100

    Theme and global styles

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import { Routes } from './Routes'
+import { theme, GlobalStyle } from './Theme'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Routes />, document.getElementById('root'))
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <>
+            <GlobalStyle />
+            <Routes />
+        </>
+    </ThemeProvider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
